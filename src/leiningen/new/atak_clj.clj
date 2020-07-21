808c903eeedcbf50b8bf0abffbc3375e816fa005
KeremAtak <kerem.atak@hotmail.com>
2020-07-22 01:31:26 +0300

    reverted custom name to atak-clj in relevant places

--- src/leiningen/new/atak_clj.clj
+++ src/leiningen/new/atak_clj.clj
@@ -1,18 +1,18 @@
-(ns leiningen.new.{{name}}
+(ns leiningen.new.atak-clj
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
             [leiningen.core.main :refer [info]]))
 
-(def render (renderer "{{name}}"))
+(def render (renderer "atak-clj"))
 
-(defn {{name}}
+(defn atak-clj
   "FIXME: write documentation"
   [name]
   (let [data {:name      name
               :sanitized (name-to-path name)}]
-    (info "Generating fresh 'lein new' {{name}} project.")
+    (info "Generating fresh 'lein new' atak-clj project.")
     (->files data
-             ["src/{{sanitized}}/core.clj" (render "src/{{name}}/core.clj" data)]
-             ["test/{{sanitized}}/core_test.clj" (render "test/{{name}}/core_test.clj" data)]
+             ["src/{{sanitized}}/core.clj" (render "src/atak-clj/core.clj" data)]
+             ["test/{{sanitized}}/core_test.clj" (render "test/atak-clj/core_test.clj" data)]
              ["project.clj" (render "project.clj" data)]
              [".gitignore" (render ".gitignore" data)]
              ["LICENSE" (render "LICENSE" data)]
